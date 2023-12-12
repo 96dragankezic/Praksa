464d68a3e09ac1d3f29d6ad505a6a989f69d4d51
Dragan_Kezic <96dragankezic@gmail.com>
2023-12-12 21:15:17 +0100

    zadatak1Duzi_a

---
+++ Library/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    
+    public static class Class1
+    {
+        public static bool funkcijaPalindrom(string recenica)
+        {
+            int duzinaStringa = recenica.Length;
+            for(int i = 0, j = duzinaStringa-1; j > duzinaStringa/2 && i < duzinaStringa/2; i++, j--)
+            {
+                if (recenica[i] != recenica[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Zadatak1Duzi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace Zadatak1Duzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite recenicu za proveru da li je palindrom: ");
+            string recenica = Console.ReadLine();
+            recenica = recenica.Trim('.', '!', '?', ' ', ',');
+            string[] reci = recenica.Split(' ', ',');
+            recenica = "";
+            for(int i = 0; i < reci.Length; i++)
+            {
+                recenica += reci[i];
+            }
+            recenica.ToLower();
+            if (Library.Class1.funkcijaPalindrom(recenica))
+            {
+                Console.WriteLine("Jeste palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("Nije palindrom!");
+            }
+
+            Console.ReadKey();
+            //Library.Class1 library = new Library.Class1();
+            //library.funkcija();
+        }
+    }
+}
