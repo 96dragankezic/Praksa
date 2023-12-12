9c8025aac9544d9aa20aa3d5a93f7bb3d3a3b5de
Dragan_Kezic <96dragankezic@gmail.com>
2023-12-12 19:45:51 +0100

    zadatak3 dorada

--- Zadatak3/Program.cs
+++ Zadatak3/Program.cs
@@ -11,23 +11,26 @@ namespace Zadatak3
     {
         static void Main(string[] args)
         {
-            int odnos = 0;
+            int odnos = 0; //jedna prosta promenljiva
             int brojBrojeva = 0;
-
+            double broj;
             Console.Write("Unesite broj brojeva koji se unosi: ");
             brojBrojeva = int.Parse(Console.ReadLine());
             while (0 < brojBrojeva--) 
             {
                 Console.Write("Unesite broj: ");
-                if (int.Parse(Console.ReadLine()) % 3 == 0)
+                broj = double.Parse(Console.ReadLine());
+                if (broj % 3 == 0 && broj % 2 == 0)
+                   odnos += 0;
+                else if (broj % 3 == 0)
                    odnos += 1;
-                else
-                   odnos -= 1;              
+                else if(broj % 2 == 0)
+                    odnos -= 1;
             }
             if (odnos > 0)
-                Console.WriteLine("Vise je celobrojnih brojeva koji su deljivi sa tri.");
+                Console.WriteLine("Vise je celobrojnih brojeva koji su deljivi sa tri za " + Math.Abs(odnos));
             else if (odnos < 0)
-                Console.WriteLine("Vise je celobrojnih brojeva deljivih sa dva.");
+                Console.WriteLine("Vise je celobrojnih brojeva deljivih sa dva za " + Math.Abs(odnos));
             else
                 Console.WriteLine("Broj celobrojnih brojeva deljivih sa tri jednak je broju celobrojnih brojeva deljivih sa dva.");
             Console.ReadKey();
