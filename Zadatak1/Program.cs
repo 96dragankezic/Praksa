f4620f5d626fcbfaccc791a32ce2a15cf025fad7
Dragan_Kezic <96dragankezic@gmail.com>
2023-12-12 15:19:41 +0100

    zadatak1

--- Zadatak1/Program.cs
+++ Zadatak1/Program.cs
@@ -8,8 +8,43 @@ namespace Zadatak1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static int izracunajFaktorijel(int brojFakt)
         {
+            int rez = 1;
+
+            while(brojFakt > 0)
+            {
+                rez *= brojFakt--;
+            }
+            
+            return rez;
+        }
+        public static int izracunajFaktorijelRekurzivno(int brojFakt)
+        {
+            int rez = brojFakt;
+
+            if (brojFakt == 1)
+                return 1;
+            rez *= izracunajFaktorijelRekurzivno(brojFakt - 1); 
+            
+            return rez;
+        }
+        public static void Main(string[] args)
+        {
+            int brojFakt = 0;
+            
+            while(true)
+            {
+                Console.Write("Unesite broj za izracunavanje faktorijela: ");
+                if (int.TryParse(Console.ReadLine(), out brojFakt))
+                {
+                    if (brojFakt > 0)
+                    {
+                        Console.WriteLine("Faktorijel unetog broja " + brojFakt + " normalno je: " + izracunajFaktorijel(brojFakt));
+                        Console.WriteLine("Faktorijel unetog broja " + brojFakt + " rekurzijom je: " + izracunajFaktorijelRekurzivno(brojFakt) + "\n");
+                    }
+                }
+            }
         }
     }
 }
