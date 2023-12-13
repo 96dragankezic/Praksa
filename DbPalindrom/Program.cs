45c1ea96262db7c177d8e1ffeb2d5b9fcf54e75f
Dragan_Kezic <96dragankezic@gmail.com>
2023-12-13 12:08:58 +0100

    zadatak 3 duzi, procitani svi redovi u tabeli Candidates

---
+++ DbPalindrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbPalindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=DESKTOP-U8FHNKB\\SQLEXPRESS;Initial Catalog=Palindromes;Integrated Security=True";
+            const string queryString = "SELECT * FROM Candidates";
+
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0]);
+                    }
+                    reader.Close();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
